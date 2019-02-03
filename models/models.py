fdab8660064733f50ab3ba6ecfa2a92b772ca832
Mohamed Magdy <moh.magdy40@gmail.com>
2019-02-03 10:43:02 +0200

    [ADD] Add social chatter in the form view.

--- models/models.py
+++ models/models.py
@@ -6,6 +6,7 @@ from odoo import models, fields, api
 class AtecEmployee(models.Model):
     _name = 'atec.employee' # Creates a table with name 'atec_employee'
     _description = 'Atec Employee'
+    _inherit = ['mail.thread']
 
     name = fields.Char(string='Employee Name', required=True, )
     email = fields.Char(string="Email", required=True)
