dd121c9074085fd7691b32fba5007a7ed660c491
Mohamed Magdy <moh.magdy40@gmail.com>
2019-02-03 14:44:12 +0200

    [ADD] Add constrains and onchange.

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class AtecEmployee(models.Model):
@@ -16,6 +17,33 @@ class AtecEmployee(models.Model):
     phone = fields.Char(string="Phone", required=False, )
     relative_ids = fields.One2many(comodel_name="res.partner", inverse_name="atec_emp_id", string="Relatives", required=False, )
     image = fields.Binary(string="Image", attachment=True, store=True, )
+
+    _sql_constraints = [
+        ('atec_emp_unique_email', 'unique(email)', 'Emails should be unique!')
+    ]
+
+    @api.constrains('email')
+    def check_email(self):
+        if self.email.endswith('gmail.com'):
+            raise UserError('Gmail is not accepted')
+
+    @api.onchange('email')
+    def onchange_email(self):
+        if self.email and self.email.endswith('gmail.com'):
+            return {
+                'warning': {
+                    'title': 'Wrong Email',
+                    'message': 'Gmail is not accepted **'
+                },
+                'value': {
+                    'website': 'http://%s' % self.email.split('@')[1],
+                },
+                # 'domain': {
+                #     'FIELD_NAME': DOMAIN
+                # }
+            }
+        if self.email:
+            self.website = 'http://%s' % self.email.split('@')[1]
 
 
 class Partner(models.Model):
